b0e25056902a280201f8b41cbd96af218bf68068
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-07-23 23:23:46 +0530

    Changed OAuth redirect URL param to prompt=consent to fetch refresh_token every time.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const oauth2Client = new google.auth.OAuth2({
 
 // Render index page.
 app.get('/', (req, res, next) => {
-  res.render('index',{
+  res.render('index', {
     serverUrl: `${process.env.BASE_URL}:${port}`
   });
 });
@@ -38,7 +38,11 @@ app.get('/', (req, res, next) => {
 app.get('/auth', (req, res, next) => {
   const url = oauth2Client.generateAuthUrl({
     access_type: 'offline',
-    prompt: 'select_account',
+    // Set to 'consent' to always re-authorize the app with Google 
+    // so that it retrieves the refresh_token everytime. 
+    // Setting it to 'select_account' will only get the refresh_token
+    // the very first time the app is authorized.
+    prompt: 'consent',
     redirect_uri: `${process.env.BASE_URL}:${port}`,
     scope: 'https://www.googleapis.com/auth/content'
   });
